efb3561cea3f1a0c612e9bff27b6ec832970e71b
Aruna Turay <tura_arun@yahoo.com>
2023-09-25 11:10:58 -0400

    all models and validations completed

--- server/models.py
+++ server/models.py
@@ -1,14 +1,52 @@
 from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.orm import validates
 
 from config import db, bcrypt
 
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
 
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False, unique=True)
+    _password_hash = db.Column(db.String)
+    image_url = db.Column(db.String)
+    bio = db.Column(db.String)
+
+    recipes = db.relationship('Recipe', backref='user')
+    
+    @hybrid_property
+    def password_hash(self):
+        raise AssertionError('Password hashes may not be viewed.')
+
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(
+            password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(
+            self._password_hash, password.encode('utf-8'))
+
+    def __repr__(self):
+        return f'User {self.username}, Image: {self.image_url}, BIO: {self.bio}'
+    
 
 class Recipe(db.Model, SerializerMixin):
     __tablename__ = 'recipes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable = False)
+    instructions = db.Column(db.String(100), nullable = False)
+    minutes_to_complete = db.Column(db.Integer)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     
-    pass
+    
+    @validates('title', 'instructions')
+    def validate_title_instructions(self, key, field):
+        assert field, f'{key} must not be empty'
+        if key == 'instructions':
+            assert len(field) >= 50, 'instructions must be at least 50 characters long'
+        return field
